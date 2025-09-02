5643682df71531358f3962144764a69d08d6e1d7
전기범 <kibeom1145@gmail.com>
2025-09-02 18:29:20 +0900

    Fix: 경고 제거

--- src/main/java/org/gentle/puma/stockup/common/security/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/gentle/puma/stockup/common/security/jwt/filter/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.gentle.puma.stockup.common.exception.CommonException;
 import org.gentle.puma.stockup.common.security.jwt.utility.JwtUtil;
@@ -28,7 +29,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
 
         String authorizationHeader = request.getHeader("Authorization");
 
