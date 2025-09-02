898953af1510e59cce659b86a0997f17a2ca2cbd
전기범 <kibeom1145@gmail.com>
2025-09-02 17:15:25 +0900

    Build: 프로젝트 세팅

---
+++ src/main/java/org/gentle/puma/stockup/StockUpApplication.java
@@ -0,0 +1,13 @@
+package org.gentle.puma.stockup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gentle/puma/stockup/StockUpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gentle.puma.stockup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
